497a362ead645821a1091daa93339515e5dfcbdd
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-03-22 11:55:20 -0400

    added type safety to accounts.ts

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,6 +3,8 @@ import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
+
 
 
 const app = new Hono()
@@ -12,7 +14,9 @@ const app = new Hono()
             const auth = getAuth(c);
 
             if (!auth?.userId) {
-                return c.json({ error: "Unauthorized" }, 401);
+                throw new HTTPException(401, {
+                    res: c.json({ error: "Unauthorized" }, 401),
+                });
             }
             
             const data = await db
