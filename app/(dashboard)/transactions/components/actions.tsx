a029fc9f52cfd2c427e3f1d58e86cc45eb5b2404
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-05 17:37:03 -0400

    added actions and category columns for transacations

---
+++ app/(dashboard)/transactions/components/actions.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { MoreHorizontal, Edit, Trash } from "lucide-react";
+
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
+import { useDeleteTransaction } from "@/features/transactions/api/use-delete-transaction";
+
+import { Button } from "@/components/ui/button";
+import { 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+ } from "@/components/ui/dropdown-menu";
+
+import { useConfirm } from "@/hooks/use-confirm";
+
+type Props = {
+    id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Delete Transaction",
+        "Are you sure you want to delete this transaction? This action cannot be undone."
+    );
+    const deleteMutation = useDeleteTransaction(id);
+    const { onOpen } = useOpenTransaction();
+
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate();
+        }
+    }
+
+    return (
+        <>
+            <ConfirmDialog />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} className="size-8 p-0">
+                        <MoreHorizontal className="size-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem disabled={deleteMutation.isPending} onClick={() => onOpen(id)}>
+                        <Edit className="mr-2 size-4" />
+                        Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem disabled={deleteMutation.isPending} onClick={handleDelete}>
+                        <Trash className="mr-2 size-4" />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+};
