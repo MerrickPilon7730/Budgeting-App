cdbecd7f2f223d94bd0da3d961875b11b20164d5
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-05 11:05:13 -0400

    Changed where I convert the amount to

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,11 +14,9 @@ export function convertMiliunitsToAmount (amount: number) {
 }
 
 export function formatCurrency(value: number) {
-  const finalValue = convertMiliunitsToAmount(value);
-
   return Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2
-  }).format(finalValue);
+  }).format(value);
 };
