d0e6c3c2cabbc334d2e302dce1cc2ac048c63bb1
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-06 11:01:39 -0400

    added summary route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+import summary from './summary'
 import accounts from './accounts'
 import categories from './categories'
 import transactions from './transactions'
@@ -14,9 +15,10 @@ const app = new Hono().basePath('/api')
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
+    .route('/summary', summary)
     .route('/accounts', accounts)
     .route('/categories', categories)
-    .route('/transactions', transactions);
+    .route('/transactions', transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
