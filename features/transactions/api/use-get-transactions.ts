01aa0c37787c3a078192fda75293754a683321f7
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-09-21 13:55:53 -0400

    updated hook

--- features/transactions/api/use-get-transactions.ts
+++ features/transactions/api/use-get-transactions.ts
@@ -1,38 +1,25 @@
 
-import {useQuery} from '@tanstack/react-query';
-import {client} from '@/lib/hono';
-import { useSearchParams } from 'next/navigation';
-import { convertMiliunitsToAmount } from '@/lib/utils';
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/hono";
+import { convertMiliunitsToAmount } from "@/lib/utils";
 
+export const useGetTransactions = (from: string, to: string, accountId: string) => {
+  return useQuery({
+    queryKey: ["transactions", { from, to, accountId }],
+    queryFn: async () => {
+      const response = await client.api.transactions.$get({
+        query: { from, to, accountId },
+      });
 
-export const useGetTransactions = () => {
-    const searchParams = useSearchParams();
-    const from = searchParams.get("from") || "";
-    const to = searchParams.get("to") || "";
-    const accountId = searchParams.get("account_id") || "";
+      if (!response.ok) {
+        throw new Error("Failed to fetch transactions");
+      }
 
-    const query = useQuery({
-        queryKey: ["transactions", {from, to, accountId}],
-        queryFn: async () => {
-            const response = await client.api.transactions.$get({
-                query: {
-                    from,
-                    to,
-                    accountId,
-                }
-            });
-            
-            if(!response.ok){ 
-                throw new Error("Failed to fetch transactions");
-            }
-
-            const {data} = await response.json();
-            return data.map((transaction) => ({
-                ...transaction,
-                amount: convertMiliunitsToAmount(transaction.amount),
-            }));
-        },
-    })
-
-    return query;
-}
+      const { data } = await response.json();
+      return data.map((transaction) => ({
+        ...transaction,
+        amount: convertMiliunitsToAmount(transaction.amount),
+      }));
+    },
+  });
+};
