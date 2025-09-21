43e3722a51c85969923db182db0782194734430e
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-09-21 13:41:41 -0400

    Added suspense

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,17 @@
 
+import { Suspense } from "react";
 import { DataGrid } from "@/components/data-table/data-grid";
 import { DataCharts } from "@/components/charts/data-charts";
-
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen mx-auto w-full pb-10 -mt-24">
-      <DataGrid />
-      <DataCharts />
+      <Suspense fallback={<div>Loading grid...</div>}>
+        <DataGrid />
+      </Suspense>
+      <Suspense fallback={<div>Loading charts...</div>}>
+        <DataCharts />
+      </Suspense>
     </div>
-
   );
 }
